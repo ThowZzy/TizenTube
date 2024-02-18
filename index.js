bca8a98f85b900025f72c9757784faccf673276f
ThowZzy <thomasoshea112001@gmail.com>
2024-02-18 23:45:47 +0100

    Revert killing of TizenTube
    Keep launcher kill tho

--- index.js
+++ index.js
@@ -7,34 +7,31 @@ let adb;
 
 async function createAdbConnection(tv_ip) {
     if (adb?._stream) {
+        adb._stream.end();
+        await sleep(300)
         adb._stream.removeAllListeners('connect');
         adb._stream.removeAllListeners('error');
         adb._stream.removeAllListeners('close');
-    }
+    } else
+        await sleep(300)
 
     adb = adbhost.createConnection({ host: tv_ip, port: 26101 });
 
     adb._stream.on('connect', () => {
         console.log('ADB connection established');
-
-        //Kill the launcher and TizenTube
+        //Kill the launcher
         const kill_job = adb.createStream(`shell:0 was_kill I80YHgsJe2.Launcher`);
         kill_job.on('data', data1 => {
             if (data1.toString().includes("spend time")) {
-                const kill_job2 = adb.createStream(`shell:0 was_kill ${Config.appId}`);
-                kill_job2.on('data', data2 => {
-                    if (data2.toString().includes("spend time")) {
-                        //Launch TizenTube in debug mode
-                        const shellCmd = adb.createStream(`shell:0 debug ${Config.appId}${Config.isTizen3 ? ' 0' : ''}`);
-                        shellCmd.on('data', data => {
-                            const dataString = data.toString();
-                            if (dataString.includes('debug')) {
-                                const port = dataString.substr(dataString.indexOf(':') + 1, 6).replace(' ', '');
-                                startDebugging(port, adb, tv_ip);
-                            }
-                        });
+                //Launch TizenTube in debug mode
+                const shellCmd = adb.createStream(`shell:0 debug ${Config.appId}${Config.isTizen3 ? ' 0' : ''}`);
+                shellCmd.on('data', data => {
+                    const dataString = data.toString();
+                    if (dataString.includes('debug')) {
+                        const port = dataString.substr(dataString.indexOf(':') + 1, 6).replace(' ', '');
+                        startDebugging(port, adb, tv_ip);
                     }
-                })
+                });
             }
         })
     });
