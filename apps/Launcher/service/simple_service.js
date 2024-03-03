bba2e4c21fb1226d339d3e22916624b753387413
ThowZzy <thomasoshea112001@gmail.com>
2024-03-03 20:49:37 +0100

    Fix service

--- apps/Launcher/service/simple_service.js
+++ apps/Launcher/service/simple_service.js
@@ -1,9 +1,12 @@
-const WebSocketServer = require('ws');
+module.exports.onStart = function () {
 
-const server = new WebSocket.Server({ port: 8081 });
+    const WebSocketServer = require('ws');
 
-server.on('connection', (ws) => {
-    ws.on('message', (msg) => {
-        ws.send(JSON.stringify({ hello: 'hello'}));
+    const server = new WebSocket.Server({ port: 8081 });
+
+    server.on('connection', (ws) => {
+        ws.on('message', (msg) => {
+            ws.send(JSON.stringify({ hello: 'hello' }));
+        });
     });
-});
+}
