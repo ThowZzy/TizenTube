05c34f2ce0bffd1592f9966e6f4ee6c1109a156a
ThowZzy <thomasoshea112001@gmail.com>
2024-03-03 19:41:55 +0100

    debug

--- apps/Launcher/script.js
+++ apps/Launcher/script.js
@@ -51,7 +51,8 @@ function start_service(){
         var pkg_id = tizen.application.getCurrentApplication().appInfo.packageId;
         var service_id = pkg_id + ".LauncherService";
         tizen.application.launchAppControl(new tizen.ApplicationControl('http://tizen.org/appcontrol/operation/service'), service_id, function () {
-            document.getElementById('text').innerText = "Service started.";
+            document.getElementById('text').innerText = "Service started..";
+            window.begin();
             connect_to_server();
         }, function (e) {
             alert('Launch Service failed: ' + e.message);
@@ -60,6 +61,7 @@ function start_service(){
 
     testWS.onopen = () => {
         document.getElementById('text').innerText = "Service is already running.";
+        window.begin();
         testWS.close();
         connect_to_server();
     };
