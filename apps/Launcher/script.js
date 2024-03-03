4d3960fa721434e0ebf9ca1f1a78c172c833e00e
ThowZzy <thomasoshea112001@gmail.com>
2024-03-03 18:14:11 +0100

    Trying the standalone stuff (oh boy)

--- apps/Launcher/script.js
+++ apps/Launcher/script.js
@@ -1,90 +1,62 @@
-//Store IP once hitting the key "finish"
-document.onkeydown = e => {
-    if (e.keyCode == 65376) {
-        var input = document.getElementById("ip");
-        localStorage.setItem("ip", input.value);
-        alert("Set IP to " + input.value);
-        window.location.reload();
-    }
-};
-
-function prompt_for_ip(previous_IP = "192.168.1.10") {
-    document.getElementById("inputbox").innerHTML = `
-    <h2 class="text2">Set Server IP which is running the node server. (Press OK to open the keyboard)</h2>
-    <h2 class="text2">You can specify the port of the server but it is optionnal, by default it will use the port 3000. Examples : 192.168.1.10, 192.168.1.10:8080 ...</h2>
-    <label class="label">Server IP:</label>
-    <input type="text" class="input" placeholder="IP Address" id="ip">
-    `;
-    // Focus on input
-    const input = document.getElementById("ip");
-    input.blur();
-    input.focus();
-
-    //Pre-fill with the previous IP or the default one
-    input.value = previous_IP;
-
-    /*input.onblur = () => { //Not sure this is needed
-        // Set IP
-        localStorage.setItem("ip", input.value);
-        alert("Set IP to " + input.value);
-        // Reload page
-        window.location.reload();
-    }*/
-}
-
 function connect_to_server() {
-    // Check if IP was set
-    if (localStorage.getItem("ip") == null) {
-        document.getElementById('text').innerText = 'Waiting for the IP of the server..';
-        // Set IP by prompt
-        prompt_for_ip();
+    const isTizen3 = navigator.userAgent.includes('Tizen 3.0');
+    var wsServer;
+    try {
+        wsServer = new WebSocket('ws://127.0.0.1:3000');
+    } catch (e) {
+        document.getElementById('text').innerText = 'Could not connect to server..';
         return;
-    } else {
-        var IP = localStorage.getItem('ip');
-        var IP_split = IP.split(":");
-        var wsServer;
-        try {
-            if (IP_split.length > 1) {
-                wsServer = new WebSocket('ws://' + IP_split[0] + ":" + IP_split[1]);
-            } else {
-                wsServer = new WebSocket('ws://' + IP_split[0] + ':3000');
-            }
-        } catch (e) {
-            document.getElementById('text').innerText = 'Could not connect to server.. Check if it is running and submit again its IP.';
-            prompt_for_ip(IP);
-            return;
-        }
-        var got_ok = false;
-        wsServer.onmessage = function (message) {
-            const msg = JSON.parse(message.data);
-            if (msg.ok) { //Improve here: SDB connection can still fail here if TV Developer IP isn't right 
-                //-> Catch this and ask user to change it (can also happen if an sdb conn already exists)
-                //tizen.application.getCurrentApplication().exit();
-                //return;
-                got_ok = true;
-                //Experimental code to address the issue
-            }
-            if(msg.exit){
-                tizen.application.getCurrentApplication().exit();
-            }
-        }
-
-        wsServer.onopen = function () {
-            wsServer.send(JSON.stringify({
-                e: 'launch'
-            }));
-        };
-
-        setTimeout(function () {
-            if (got_ok) {
-                document.getElementById('text').innerText = "Connection with the server was successful but the ADB connection failed... You likely did not change the TV's developer IP to your server's. Or you still have an active connection to your TV with tizen device manager. Or you need to enable/disable the Tizen3 Compatibility.";
-            } else{
-                document.getElementById('text').innerText = 'Could not connect to server after 10 seconds.. Check if it is running and submit again its IP.';
-            }
-            prompt_for_ip(IP);
-            return;
-        }, 10000);
     }
+    var got_ok = false;
+    wsServer.onmessage = function (message) {
+        const msg = JSON.parse(message.data);
+        if (msg.ok) { //Improve here: SDB connection can still fail here if TV Developer IP isn't right 
+            //-> Catch this and ask user to change it (can also happen if an sdb conn already exists)
+            //tizen.application.getCurrentApplication().exit();
+            //return;
+            got_ok = true;
+            //Experimental code to address the issue
+        }
+        if (msg.exit) {
+            tizen.application.getCurrentApplication().exit();
+        }
+    }
+
+    wsServer.onopen = function () {
+        wsServer.send(JSON.stringify({
+            e: 'launch',
+            version: isTizen3
+        }));
+    };
+
+    setTimeout(function () {
+        if (got_ok) {
+            document.getElementById('text').innerText = "Connection with the server was successful but the ADB connection failed... You likely did not change the TV's developer IP to your server's. Or you still have an active connection to your TV with tizen device manager. Or you need to enable/disable the Tizen3 Compatibility.";
+        } else {
+            document.getElementById('text').innerText = 'Could not connect to server after 10 seconds.. Check if it is running and submit again its IP.';
+        }
+        return;
+    }, 10000);
 }
 
-document.addEventListener("DOMContentLoaded", connect_to_server);
+function start_service(){
+    var testWS = new WebSocket(`ws://127.0.0.1:3000`);
+    testWS.onerror = () => {
+        var pkg_id = tizen.application.getCurrentApplication().appInfo.packageId;
+        var service_id = pkg_id + ".LauncherService";
+        tizen.application.launchAppControl(new tizen.ApplicationControl('http://tizen.org/appcontrol/operation/service'), service_id, function () {
+            document.getElementById('text').innerText = "Service started.";
+            connect_to_server();
+        }, function (e) {
+            alert('Launch Service failed: ' + e.message);
+        });
+    };
+
+    testWS.onopen = () => {
+        document.getElementById('text').innerText = "Service is already running.";
+        testWS.close();
+        connect_to_server();
+    };
+}
+
+document.addEventListener("DOMContentLoaded", start_service);
---
+++ apps/Launcher/service/utils.js
@@ -0,0 +1,25 @@
+export function log(message, additional_msg = "") {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+    const timestamp = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    if (additional_msg)
+        console.log(`[${timestamp}] ${message}`, additional_msg);
+    else
+        console.log(`[${timestamp}] ${message}`);
+}
+
+export function log_error(message, error = "") {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+    const timestamp = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    if (error)
+        console.error(`[${timestamp}] [Error] ${message}`, error);
+    else
+        console.error(`[${timestamp}] [Error] ${message}`);
+}
