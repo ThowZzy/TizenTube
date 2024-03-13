5c711caba9da149e5459a9136defc445ccc7a9e0
ThowZzy <thomasoshea112001@gmail.com>
2024-03-13 16:26:07 +0100

    Revert "fixing.."
    This reverts commit 368d62a7da33e9ff43f46b7202b10a06e2e31fa6.

--- apps/Launcher/service/simple_service.js
+++
@@ -1,9 +0,0 @@
-const WebSocketServer = require('ws');
-
-const server = new WebSocket.Server({ port: 8081 });
-
-server.on('connection', (ws) => {
-    ws.on('message', (msg) => {
-        ws.send(JSON.stringify({ hello: 'hello'}));
-    });
-});
